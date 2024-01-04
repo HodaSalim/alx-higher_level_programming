02945826d8842852a8e23ed2291186b45a5c1eae
Hoda Salim <hoda.salim11@gmail.com>
2024-01-04 23:12:34 +0200

    script that writes a string to a file

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const process = require("process");
+const fs = require("fs");
+
+const file = process.argv[2];
+const data = process.argv[3];
+
+fs.writeFile(file, data, "utf-8", (err, data) => {
+  if (err) console.log(err);
+});
