9cbd4b89d0453ec94487c8d0f944ad30cde828b7
Hoda Salim <hoda.salim11@gmail.com>
2024-01-04 22:58:16 +0200

    FIX: Semistandard

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-const process = require("node:process");
-const fs = require("node:fs");
+const process = require('node:process');
+const fs = require('node:fs');
 // argument index is 2
 const file = process.argv[2];
-fs.readFile(file, "utf-8", (err, data) => {
+fs.readFile(file, 'utf-8', (err, data) => {
   if (err) console.log(err);
   console.log(data);
 });
