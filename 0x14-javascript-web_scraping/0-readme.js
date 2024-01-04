c4e64b412e7e72696bda98377052e42cc240abba
Hoda Salim <hoda.salim11@gmail.com>
2024-01-04 22:56:29 +0200

    Let's Pray

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,9 @@
-#!/usr/bin/node
+#!/usr/bin/env node
 const process = require("node:process");
 const fs = require("node:fs");
 // argument index is 2
 const file = process.argv[2];
-fs.readFile(file, { encoding: "utf-8" }, (err, data) => {
+fs.readFile(file, "utf-8", (err, data) => {
   if (err) console.log(err);
   console.log(data);
 });
