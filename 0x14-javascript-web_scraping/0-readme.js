d8e61d0feaac5a33cfc3b6cfd6ae2f7efffce960
Hoda Salim <hoda.salim11@gmail.com>
2024-01-04 22:46:02 +0200

    FIX: error handeling

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,7 +4,7 @@ const fs = require("node:fs");
 // argument index is 2
 const file = process.argv[2];
 fs.readFile(file, { encoding: "utf-8" }, (err, data) => {
-  if (err) throw err;
-  console.log(data[1]);
+  if (err) console.log(err);
+  console.log(data);
 });
 console.log(file);
